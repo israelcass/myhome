9cd8ec3d1ee2f5f30cbe1ee4232d5e42cec4589e
enzomercanti <enzomercanti@gmail.com>
2019-01-26 17:07:56 -0200

    First open and close door mechanics

---
+++ Assets/Animations/DoorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+	Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+		anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			anim.SetTrigger ("OpenDoor");
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			anim.enabled = true;
+		}
+	}
+
+	void pauseAnimationEvent() {
+		anim.enabled = false;
+	}
+}
