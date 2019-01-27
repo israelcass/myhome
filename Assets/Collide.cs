99a89f4b3497cc7ab40c3f11b407fe0deadcdf5a
enzomercanti <enzomercanti@gmail.com>
2019-01-27 16:11:15 -0200

    Polishing

---
+++ Assets/Collide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collide : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "lava") {
+			SceneManager.LoadScene("DeathMenu");
+		} else if (other.tag == "jar") {
+			SceneManager.LoadScene("WinMenu");
+		}
+	}
+}
